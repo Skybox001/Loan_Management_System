ac8bcc16beec752df620c50d78836a8378f7a306
Garv <garvrawatt@gmail.com>
2026-08-19 14:11:50 +0530

    Support NEXT_PUBLIC_API_URL in frontend/lib/api.ts with local fallback

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,11 +2,12 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 
 api.interceptors.request.use((config) => {
   const token = Cookies.get("access_token");
